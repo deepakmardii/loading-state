bfae7f6debf2400f64de5c5b5244d3ae983fa270
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-17 07:40:04 +0530

    simple loadin state with routing

---
+++ app/SkillsSection/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SkillsSection = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      Skills Section
+    </div>
+  );
+};
+
+export default SkillsSection;
